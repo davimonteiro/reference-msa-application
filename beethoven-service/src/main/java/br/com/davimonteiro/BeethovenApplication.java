8384b010c33d2344fc1ace957739e2df20fa13e5
Davi Monteiro Barbosa <davimonteiro.ce@gmail.com>
2018-06-11 00:11:16 -0300

    Enable eureka client.

--- beethoven-service/src/main/java/br/com/davimonteiro/BeethovenApplication.java
+++ beethoven-service/src/main/java/br/com/davimonteiro/BeethovenApplication.java
@@ -5,8 +5,10 @@ import io.beethoven.Beethoven;
 import io.beethoven.config.EnableBeethoven;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.ApplicationContext;
 
+@EnableEurekaClient
 @EnableBeethoven
 @SpringBootApplication
 public class BeethovenApplication {

c5de939b856966c4dd356585d230006167ac222f
Davi Monteiro Barbosa <davimonteiro.ce@gmail.com>
2018-06-13 23:11:35 -0300

    Code refactoring.

--- inventory-service/src/main/java/demo/v1/InventoryControllerV1.java
+++ inventory-service/src/main/java/demo/v1/InventoryControllerV1.java
@@ -2,11 +2,13 @@ package demo.v1;
 
 import demo.domain.ShoppingCart;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import static java.util.Optional.ofNullable;
 import static org.springframework.http.HttpStatus.NOT_FOUND;
+import static org.springframework.http.MediaType.*;
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
 import static org.springframework.web.bind.annotation.RequestMethod.POST;
 
@@ -17,7 +19,7 @@ public class InventoryControllerV1 {
     @Autowired
     private InventoryServiceV1 inventoryService;
 
-    @RequestMapping(path = "/catalogs/{catalogNumber}", name = "findCatalogByCatalogNumber", method = GET)
+    @GetMapping(path = "/catalogs/{catalogNumber}", name = "findCatalogByCatalogNumber")
     public ResponseEntity findCatalogByCatalogNumber(@PathVariable("catalogNumber") Long catalogNumber) {
         return ofNullable(inventoryService.findCatalogByCatalogNumber(catalogNumber))
                 .map(ResponseEntity::ok)
@@ -25,28 +27,28 @@ public class InventoryControllerV1 {
 
     }
 
-    @RequestMapping(path = "/products/{productId}", name = "getProduct", method = GET)
+    @GetMapping(path = "/products/{productId}", name = "getProduct")
     public ResponseEntity getProduct(@PathVariable("productId") String productId) {
         return ofNullable(inventoryService.getProduct(productId))
                 .map(ResponseEntity::ok)
                 .orElse(new ResponseEntity(NOT_FOUND));
     }
 
-    @RequestMapping(path = "/inventory", name = "getAvailableInventoryForProductIds", method = GET)
+    @GetMapping(path = "/inventory", name = "getAvailableInventoryForProductIds")
     public ResponseEntity getAvailableInventoryForProductIds(@RequestParam("productIds") String productIds) {
         return ofNullable(inventoryService.getAvailableInventoryForProductIds(productIds))
                 .map(ResponseEntity::ok)
                 .orElse(new ResponseEntity(NOT_FOUND));
     }
 
-    @RequestMapping(path = "/inventory/orchestrated", method = GET)
+    @GetMapping(path = "/inventory/orchestrated")
     public ResponseEntity getAvailableInventoryForProductIdsOrchestrated(@RequestParam("shoppingCart") ShoppingCart shoppingCart) {
         return ofNullable(inventoryService.getAvailableInventoryForProductIds(shoppingCart))
                 .map(ResponseEntity::ok)
                 .orElse(new ResponseEntity(NOT_FOUND));
     }
 
-    @RequestMapping(path = "/inventory/checkavailable/orchestrated", method = POST)
+    @PostMapping(path = "/inventory/checkavailable/orchestrated", consumes = APPLICATION_JSON_VALUE)
     public ResponseEntity checkAvailableInventory(@RequestBody ShoppingCart shoppingCart) throws Exception {
         return ofNullable(inventoryService.checkAvailableInventory(shoppingCart))
                 .map(ResponseEntity::ok)

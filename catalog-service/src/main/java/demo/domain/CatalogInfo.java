add5cf398d3ca1eeba4f672c5379746d1f727cd7
Davi Monteiro Barbosa <davimonteiro.ce@gmail.com>
2018-10-20 12:50:30 -0300

    Adding @GeneratedValue.

--- catalog-service/src/main/java/demo/domain/CatalogInfo.java
+++ catalog-service/src/main/java/demo/domain/CatalogInfo.java
@@ -3,15 +3,14 @@ package demo.domain;
 import lombok.Data;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-import java.io.Serializable;
-import java.util.UUID;
 
 @Data
 @Entity
-public class CatalogInfo implements Serializable {
+public class CatalogInfo {
 
-    @Id
+    @Id @GeneratedValue
     private Long id;
     private Long catalogId;
     private Boolean active;

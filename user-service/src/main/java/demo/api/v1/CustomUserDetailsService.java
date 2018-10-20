a1dd242764541298fc76398433ff932d7708ec24
Davi Monteiro Barbosa <davimonteiro.ce@gmail.com>
2018-10-20 12:54:33 -0300

    Implementing UserDetails.

---
+++ user-service/src/main/java/demo/api/v1/CustomUserDetailsService.java
@@ -0,0 +1,27 @@
+package demo.api.v1;
+
+import demo.domain.User;
+import demo.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findUserByUsername(username);
+
+        if (user == null) {
+            throw new UsernameNotFoundException("User not found.");
+        }
+
+        return user;
+    }
+}
--- user-service/src/main/java/demo/domain/User.java
+++ user-service/src/main/java/demo/domain/User.java
@@ -1,37 +1,56 @@
 package demo.domain;
 
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 
 @Data
 @Entity
-public class User {
+public class User implements UserDetails {
 
-    @Id
-    @GeneratedValue
+    @Id @GeneratedValue
     private Long id;
     private String username;
+    private String password;
     private String firstName;
     private String lastName;
     private String email;
 
-    public User() { }
-
-    public User(String username, String firstName, String lastName, String email) {
-        this.username = username;
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.email = email;
+    @Override
+    public Collection getAuthorities() {
+        List<GrantedAuthority> authorities = new ArrayList();
+        authorities.add(new SimpleGrantedAuthority("ROLE_USER"));
+        return authorities;
     }
 
-    public User(String firstName, String lastName, String email) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.email = email;
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
     }
 
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
 }

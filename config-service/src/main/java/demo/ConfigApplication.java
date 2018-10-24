5426aa4950d2073c0bd94e68c0bfe971d441de37
Davi Monteiro Barbosa <davimonteiro.ce@gmail.com>
2018-10-24 19:58:14 -0300

    Refactoring and documenting config-service.

--- config-service/src/main/java/demo/ConfigApplication.java
+++ config-service/src/main/java/demo/ConfigApplication.java
@@ -4,6 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
+/**
+ * The {@link ConfigApplication} is a cloud-native Spring Boot application for providing
+ * server and client-side support for externalized configuration in a distributed system.
+ *
+ * @author Kenny Bastani
+ * @author Josh Long
+ * @author Davi Monteiro
+ */
 @SpringBootApplication
 @EnableConfigServer
 public class ConfigApplication {
@@ -11,4 +19,5 @@ public class ConfigApplication {
     public static void main(String[] args) {
         SpringApplication.run(ConfigApplication.class, args);
     }
+
 }

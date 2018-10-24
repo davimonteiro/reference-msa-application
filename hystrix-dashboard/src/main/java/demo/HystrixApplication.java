721f6843d8711365ff684eb2a01585e8c40f07f4
Davi Monteiro Barbosa <davimonteiro.ce@gmail.com>
2018-10-24 20:39:56 -0300

    Refactoring and documenting hystrix-dashboard.

--- hystrix-dashboard/src/main/java/demo/HystrixApplication.java
+++ hystrix-dashboard/src/main/java/demo/HystrixApplication.java
@@ -4,6 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
+
+/**
+ * The {@link HystrixApplication} is a cloud-native Spring Boot application for providing
+ * a dashboard for monitoring applications using Hystrix.
+ *
+ * @author Kenny Bastani
+ * @author Josh Long
+ * @author Davi Monteiro
+ */
 @SpringBootApplication
 @EnableHystrixDashboard
 public class HystrixApplication {
@@ -11,4 +20,5 @@ public class HystrixApplication {
     public static void main(String[] args) {
         SpringApplication.run(HystrixApplication.class, args);
     }
+
 }

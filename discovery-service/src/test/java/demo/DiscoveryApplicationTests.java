3846d4de0d5943e6724bcf69af7fc5a943b1b48b
Davi Monteiro Barbosa <davimonteiro.ce@gmail.com>
2018-10-24 19:19:10 -0300

    Loading test for DiscoveryApplication.

--- discovery-service/src/test/java/demo/DiscoveryApplicationTests.java
+++ discovery-service/src/test/java/demo/DiscoveryApplicationTests.java
@@ -1,18 +1,18 @@
-/*
 package demo;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringBootTest
-public class ApplicationTests {
+@SpringBootTest(classes = DiscoveryApplication.class)
+@ActiveProfiles(profiles = "test")
+public class DiscoveryApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Test
+    public void contextLoads() {
+    }
 
 }
-*/

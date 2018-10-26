ed023c3cbb23b313697135936694c09e53444234
Davi Monteiro Barbosa <davimonteiro.ce@gmail.com>
2018-10-26 10:44:28 -0300

    Fix build issues and add a new docker plugin version.

--- run.sh
+++ run.sh
@@ -16,15 +16,15 @@ docker-compose stop
 docker-compose rm -f
 
 # Start the config service first and wait for it to become available
-#docker-compose up -d config-service
+docker-compose up -d config-service
 
-#while [ -z ${CONFIG_SERVICE_READY} ]; do
-#  echo "Waiting for config service..."
-#  if [ "$(curl --silent $DOCKER_IP:8888/actuator/health 2>&1 | grep -q '\"status\":\"UP\"'; echo $?)" = 0 ]; then
-#      CONFIG_SERVICE_READY=true;
-#  fi
-#  sleep 2
-#done
+while [ -z ${CONFIG_SERVICE_READY} ]; do
+  echo "Waiting for config service..."
+  if [ "$(curl --silent $DOCKER_IP:8888/actuator/health 2>&1 | grep -q '\"status\":\"UP\"'; echo $?)" = 0 ]; then
+      CONFIG_SERVICE_READY=true;
+  fi
+  sleep 2
+done
 
 # Start the discovery service next and wait
 docker-compose up -d discovery-service

71fc7b5c76b54e0d6620969f93115e9eba179b42
Davi Monteiro Barbosa <davimonteiro.ce@gmail.com>
2018-10-29 12:00:37 -0300

    Removing auto configuration for the DataSourceHealthIndicatorAutoConfiguration.

--- user-service/src/main/java/demo/UserApplication.java
+++ user-service/src/main/java/demo/UserApplication.java
@@ -2,6 +2,7 @@ package demo;
 
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.jdbc.DataSourceHealthIndicatorAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
@@ -20,8 +21,9 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
  *
  * @author Kenny Bastani
  * @author Josh Long
+ * @author Davi Monteiro
  */
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceHealthIndicatorAutoConfiguration.class)
 @EnableJpaAuditing
 @EnableJpaRepositories
 @EnableEurekaClient

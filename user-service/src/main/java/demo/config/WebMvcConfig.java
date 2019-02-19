01c3340ad386e1c5418171b9b3f61d3922988e5b
davimonteiro <davimonteiro.ce@gmail.com>
2019-02-19 18:02:18 +0000

    Removing imports.

--- user-service/src/main/java/demo/config/WebMvcConfig.java
+++ user-service/src/main/java/demo/config/WebMvcConfig.java
@@ -1,7 +1,6 @@
 package demo.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.Ordered;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
